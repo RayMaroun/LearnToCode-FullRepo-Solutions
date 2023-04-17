46a58e69c47aba30ddcd7b7f7a32708edb6a03ce
RayMaroun <raymondmaroun15@gmail.com>
2023-04-17 19:14:42 +0300

    Add workbook2 and mod01

---
+++ LearnToCode/Workbook2/Mod01/FullNameGenerator/src/main/java/com/learntocode/FullNameGenerator.java
@@ -0,0 +1,42 @@
+package com.learntocode;
+
+import java.util.Scanner;
+
+public class FullNameGenerator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name:");
+
+        System.out.print("First name: ");
+        String firstName = scanner.nextLine().trim();
+
+        System.out.print("Middle name: ");
+        String middleName = scanner.nextLine().trim();
+
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine().trim();
+
+        System.out.print("Suffix: ");
+        String suffix = scanner.nextLine().trim();
+
+        String fullName = firstName;
+
+        if (!middleName.isEmpty()) {
+            fullName += " " + middleName.charAt(0) + ".";
+        }
+
+        fullName += " " + lastName;
+
+        if (!suffix.isEmpty()) {
+            fullName += ", " + suffix;
+        }
+
+        System.out.println("\nFull name: " + fullName);
+
+    }
+
+}
+
