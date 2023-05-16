36ec3b3b25cbba912565eec758c96247ecd5eb72
RayMaroun <raymondmaroun15@gmail.com>
2023-05-16 17:31:13 +0300

    Add workbook5 mod02

---
+++ LearnToCode/Workbook5/Mod02/Vehicles/src/main/java/com/learntocode/Moped.java
@@ -0,0 +1,17 @@
+package com.learntocode;
+
+public class Moped extends Vehicle {
+    private int maxWeight;
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public void ride() {
+        System.out.println("Riding the moped.");
+    }
+}
