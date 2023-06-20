306c994374a1fdeb7e9e53a4723dd6083bc739a2
RayMaroun <raymondmaroun15@gmail.com>
2023-06-20 22:34:00 +0300

    Add workbook 9 mod02

---
+++ LearnToCode/Workbook9/Mod02/NorthwindTraders2/src/test/java/org/yearup/NorthwindTraders/NorthwindTradersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yearup.NorthwindTraders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
