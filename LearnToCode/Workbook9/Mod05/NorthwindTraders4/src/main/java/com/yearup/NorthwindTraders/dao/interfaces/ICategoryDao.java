0058f22b8440c481b19078107f48ee52a8759498
RayMaroun <raymondmaroun15@gmail.com>
2023-06-23 18:02:48 +0300

    Update workbook9 mod05

---
+++ LearnToCode/Workbook9/Mod05/NorthwindTraders4/src/main/java/com/yearup/NorthwindTraders/dao/interfaces/ICategoryDao.java
@@ -0,0 +1,12 @@
+package com.yearup.NorthwindTraders.dao.interfaces;
+
+import com.yearup.NorthwindTraders.models.Category;
+
+import java.util.List;
+
+public interface ICategoryDao {
+    List<Category> getAll();
+    Category getById(int id);
+
+    Category insert(Category category);
+}
